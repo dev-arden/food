8177eb6783246c56e5aff0d1d84b292bacfa9902
smulionOong <1224police@likelion.org>
2020-10-08 17:00:04 +0900

    making Request

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -5,8 +5,6 @@ export default axios.create({
   headers: {
     Authorization: 'Bearer mmUGdg5m1sXVEy-B1oYSeyHcHJjRGW6kbqYe_WBCh7a-WegWyFOYQoVBhHXRHk2GVqrbC2u536jrBeIXr_wRnSgxhMbKuUuOLDb3WJi_OzHdujVelhqXBM7BrJB8X3Yx'
   }
-  //yelp.get('/search') baseurl을 설정함으로써 이제 이런식으로
-  //부를 수 있다
 });
 
 
